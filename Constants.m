c9881de5d2112f94e1fae53d040117e08ae04830
dforrer <dforrer@gmail.com>
2014-05-28 21:36:33 +0200

    Increased MAX_CONCURRENT_DOWNLOADS to 32

--- Constants.m
+++ Constants.m
@@ -7,6 +7,6 @@
 
 NSString * const APP_NAME = @"Peerbox";
 int const MAX_REVS_PER_REQUEST = 400;
-int const MAX_CONCURRENT_DOWNLOADS	= 16;
+int const MAX_CONCURRENT_DOWNLOADS	= 32;
 BOOL const IGNORE_RESOURCE_FORKS = FALSE;
 int const MAX_VERSIONS_PER_FILE = 100;
