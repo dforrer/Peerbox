f0c34b2586274ed788497450e14aa96fe2975f85
Daniel Forrer <Daniel@macbookair.local>
2014-08-05 21:54:21 +0200

    Revert "Test with RNEncryptor instead RNDecryptor"
    This reverts commit 1cc224d4ce55c89469bf61eaab4a49b6c9a719d9.

--- DownloadFile.m
+++ DownloadFile.m
@@ -29,7 +29,7 @@
 
 @implementation DownloadFile
 {
-	RNEncryptor * decryptor;
+	RNDecryptor * decryptor;
 	NSURLConnection * connection;
 	CC_SHA1_CTX state;
 	uint64_t downloadSize;
@@ -75,7 +75,7 @@
 		//	DebugLog(@"URL: %@", [request URL]);
 		
 		
-		decryptor = [[RNEncryptor alloc] initWithSettings:kRNCryptorAES256Settings password:[[[rev peer] share] secret] handler:
+		decryptor = [[RNDecryptor alloc] initWithPassword:[[[rev peer] share] secret] handler:
 				   ^(RNCryptor *cryptor, NSData *data) {
 					   
 					   //DebugLog(@"DownloadFile: 5. Decryptor-Handler");
